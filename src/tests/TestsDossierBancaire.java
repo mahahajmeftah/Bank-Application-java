f4c748a4fbc415d4e1ae7c0e56473ee005becf12
usrlocal <usrlocal@GA319-FE19.ad.univ-angers.fr>
2023-01-09 15:12:07 +0100

    3 tests

--- src/tests/TestsDossierBancaire.java
+++ src/tests/TestsDossierBancaire.java
@@ -1,14 +1,36 @@
 package tests;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestsDossierBancaire {
+import myPackage.DossierBancaire;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class TestsDossierBancaire {
+	 
+
+	
+	 @Test
+		public void testConstructeur() 
+		{
+			DossierBancaire dossier=new DossierBancaire();
+			
+			assertEquals(0,dossier.get_solde(),0.0001); //voir documentation en ligne sur assertions Junit 
+		}
+	 @Test
+	public void testDeposer() 
+	{
+		DossierBancaire dossier=new DossierBancaire();
+		dossier.deposer(100);
+		assertEquals(100,dossier.get_solde(),0); //voir documentation en ligne sur assertions Junit 
 	}
+	 @Test
+		public void testRenumerer() 
+		{
+			DossierBancaire dossier=new DossierBancaire();
+			dossier.deposer(100);
+			dossier.remunerer();
+			double solde_expected = 100*(1+3.2);
+			assertEquals(solde_expected,dossier.get_solde(),0.001); //voir documentation en ligne sur assertions Junit 
+		}
 
 }
