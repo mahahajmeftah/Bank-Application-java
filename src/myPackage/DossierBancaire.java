67564494273e8739fb62b1de92a4bb7e946451dd
usrlocal <usrlocal@GA319-FE19.ad.univ-angers.fr>
2023-01-09 16:31:48 +0100

    modification de constructeur de DossierBancaire

--- src/myPackage/DossierBancaire.java
+++ src/myPackage/DossierBancaire.java
@@ -1,16 +1,32 @@
 package myPackage;
 
 public class DossierBancaire {
+	private CompteCourant cc;
+	private CompteEpargne ep;
+	private double m_solde;
+	
 	
 	//Constructeur
     public DossierBancaire()
     {
     	m_solde=0;
+    	cc =new CompteCourant(m_solde);
+    	ep = new CompteEpargne(m_solde); 
     }
 
-    public void deposer(double value) {m_solde+=value;}
-    public double get_solde() {return m_solde;}
-    public void remunerer() {}
+    public void deposer(double value) {
+    	cc.deposerC(value*0.4);
+    	ep.deposerE(value*0.6);
+    	m_solde+=value;
+    	}
+    public double get_solde() {
+    	return cc.getSolde()+ep.getSolde() ;
+    	}
+    public void remunerer() {
+    	
+    	ep.renumererE();
+    	
+    }
 	
-    private double m_solde;
+    
 }
