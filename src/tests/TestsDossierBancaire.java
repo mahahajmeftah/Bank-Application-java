e01136c30abd64e15bc87319ac56c13f49a7c0bd
usrlocal <usrlocal@GA319-FE19.ad.univ-angers.fr>
2023-01-09 15:10:14 +0100

    clean unit tests

---
+++ src/tests/TestsDossierBancaire.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestsDossierBancaire {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
