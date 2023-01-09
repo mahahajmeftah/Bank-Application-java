0921654ac2cecf77c412e7e5a657ef6b164b7565
mhadjmeftah <hajmeftahmaha@gmail.com>
2023-01-09 17:52:51 +0100

    client banquier

--- src/myPackage/Banquier.java
+++ src/myPackage/Banquier.java
@@ -1,5 +1,10 @@
 package myPackage;
 
-public class Banquier {
+public class Banquier extends Personne {
+
+	public Banquier(String nom, String prenom) {
+		super(nom, prenom);
+		// TODO Auto-generated constructor stub
+	}
 
 }
