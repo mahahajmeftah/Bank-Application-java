dbc8c71fe399a06cdd8d4df0d58c9126e97ac340
usrlocal <usrlocal@GE22-FE11.ad.univ-angers.fr>
2023-01-13 11:20:53 +0100

    1. Ajoutez la possibilité de retirer de l’argent du dossier bancaire : on considérera qu’un tel retrait n’altère que la classe «Compte Courant», et qu’une exception est levée si le solde est insuffisant. 2. Faites en sorte que la levée de cette exception soit vérifiée par le test unitaire associée

--- src/tests/TestsDossierBancaire.java
+++ src/tests/TestsDossierBancaire.java
@@ -33,6 +33,24 @@ public class TestsDossierBancaire {
 			System.out.println(dossier.get_solde());
 			assertEquals(solde_expected,dossier.get_solde(),0.001); //voir documentation en ligne sur assertions Junit 
 		}
+	 
+	 @Test
+		public void testRetirerSuccess() throws Exception {
+		 DossierBancaire dossier=new DossierBancaire();
+			dossier.deposer(50);
+			dossier.retirer(50*0.4);
+			assertEquals(30, dossier.get_solde(), 0.00);
+			
+			
+		}
+	 @Test
+		public void testRetirerInsufficientFunds() throws Exception {
+		 DossierBancaire dossier=new DossierBancaire();	
+			dossier.retirer(200);
+			Exception exception = assertThrows(Exception.class, () -> dossier.retirer(200));
+			assertEquals("Insufficient funds", exception.getMessage());
+		
+		}
 	
 
 }
